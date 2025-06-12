9005684cc227b08d5f7e1a66fea7700a3092769a
Cruz Flores <cruz.tag@gmail.com>
2025-06-11 19:59:43 -0600

    fix: update greeting message in index.js and add Render.com deploy step in docker-build.yml

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello cat nieves World!</h1>');
+  res.send('<h1>Hola mundo gatuno!</h1>');
 });
 
 const PORT = 5050;
